002b0df7f24665afdb1a2c7197bafd76424ed272
Blaze3 <85939303+christopherwibisono@users.noreply.github.com>
2021-12-03 22:38:23 +0700

    Add files via upload

---
+++ Factory/CoftFactory.cs
@@ -0,0 +1,20 @@
+﻿using Coft_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coft_Project.Factory
+{
+    public class CoftFactory
+    {
+        public static User createUser(string mail, string pass)
+        {
+            User users = new User();
+            users.userEmail = mail;
+            users.userPassword = pass;
+            users.userMemship = "Member";
+            return users;
+        }
+    }
+}
